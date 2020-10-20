a1aca388dd856f813d2856a7746638ae8699d676
GitHub Action <action@github.com>
2020-10-20 06:25:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/mattr/svgsourcenavigation/MyBundle.kt
+++ src/main/kotlin/com/github/mattr/svgsourcenavigation/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mattr.svgsourcenavigation
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mattr/svgsourcenavigation/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mattr/svgsourcenavigation/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mattr.svgsourcenavigation.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mattr.svgsourcenavigation.MyBundle
 
 class MyApplicationService {
 
